b3a7da600441342ff51e3c94c71b255174a8667a
aip <ariful.islamk777@gmail.com>
2021-06-01 19:53:08 +0600

    Created country models

---
+++ appzss/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django_countries.fields import CountryField    
+import pytz
+
+# Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=2, choices=pytz.country_names.items())
+    latitude = models.FloatField(max_length=150, blank=False)
+    longitude = models.FloatField(max_length=150, blank=False)
+    code = models.CharField(max_length=5, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        self.code = self.name
+        super(Country, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
