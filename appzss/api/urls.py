146d1e32a0234f03abd16138f82c8eae55d16be3
aip <ariful.islamk777@gmail.com>
2021-06-06 01:32:49 +0600

    Add address API Url

--- appzss/api/urls.py
+++ appzss/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from appzss.api.country_views import CountryListAPI, CountryDetailAPIView
 from appzss.api.state_view import StateListAPI, StateDetailAPIView
-
+from appzss.api.address_view import AddressListAPI, AddressDetailAPIView
 urlpatterns = [
     path('country/', CountryListAPI.as_view()),
     path('country/<int:pk>/', CountryDetailAPIView.as_view()),
 
     path('state/', StateListAPI.as_view()),
     path('state/<int:pk>/', StateDetailAPIView.as_view()),
- 
+
+    path('address/', AddressListAPI.as_view()),
+    path('address/<int:pk>/', AddressDetailAPIView.as_view()),
 ]
